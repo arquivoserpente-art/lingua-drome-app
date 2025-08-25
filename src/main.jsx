2059c50a92cfb2a8d3576bd246375bc802437e84
arquivoserpente-art <arquivoserpente@gmail.com>
2025-08-25 19:10:24 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import DromeSite from "./DromeSite";
-
+import "./index.css"; // <-- IMPORTANTE: ativa o Tailwind
 createRoot(document.getElementById("root")).render(<DromeSite />);
