511268485ae3cda687e591079a1ec2487eb418eb
arquivoserpente-art <arquivoserpente@gmail.com>
2025-08-25 18:49:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import DromeSite from "./DromeSite";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
-
+createRoot(document.getElementById("root")).render(<DromeSite />);
